6256799634f4b178b19c68fee5416fa4c87d7ccc
Ido Avraham <idoavraham@idf.il>
2023-12-29 16:00:41 +0200

    feat: fingerprint distinct users (without exposing identity or PII)

--- tftui/apis.py
+++ tftui/apis.py
@@ -1,13 +1,16 @@
-import uuid
+import platform
+import socket
+import hashlib
 import importlib.metadata
 import requests
 from posthog import Posthog
+from tftui.constants import nouns, adjectives
 
 
 class OutboundAPIs:
     is_new_version_available = False
     is_usage_tracking_enabled = True
-    session_id = uuid.uuid4()
+    generated_handle = None
     version = importlib.metadata.version("tftui")
 
     POSTHOG_API_KEY = "phc_tjGzx7V6Y85JdNfOFWxQLXo5wtUs6MeVLvoVfybqz09"  # + "uncomment-while-developing"
@@ -25,13 +28,27 @@ class OutboundAPIs:
             is_new_version_available = True
 
     @staticmethod
+    def generate_handle():
+        fingerprint_data = f"{platform.system()}-{platform.node()}-{platform.release()}-{socket.gethostname()}"
+        fingerprint = int(hashlib.sha256(fingerprint_data.encode()).hexdigest(), 16)
+        OutboundAPIs.generated_handle = (
+            adjectives[fingerprint % len(adjectives)]
+            + " "
+            + nouns[fingerprint % len(nouns)]
+        )
+
+    @staticmethod
     def post_usage(message: str) -> None:
-        if OutboundAPIs.is_usage_tracking_enabled:
-            OutboundAPIs.posthog.capture(
-                OutboundAPIs.session_id,
-                message,
-                {"tftui_version": OutboundAPIs.version},
-            )
+        if not OutboundAPIs.is_usage_tracking_enabled:
+            return
+        if not OutboundAPIs.generated_handle:
+            OutboundAPIs.generate_handle()
+
+        OutboundAPIs.posthog.capture(
+            OutboundAPIs.generated_handle,
+            message,
+            {"tftui_version": OutboundAPIs.version},
+        )
 
     @staticmethod
     def disable_usage_tracking() -> None:
